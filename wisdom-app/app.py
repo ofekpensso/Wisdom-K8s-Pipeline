d461be4854d06fc2a632b9088cfa9a0b8ae98ca0
ofekpenso@gmail.com <ubuntu@ip-172-31-86-174.ec2.internal>
2026-01-22 14:56:11 +0000

    check web hook

--- wisdom-app/app.py
+++ wisdom-app/app.py
@@ -15,7 +15,7 @@ wisdoms = [
 @app.route('/')
 def get_wisdom():
     host_name = socket.gethostname()
-    return f"<div style='background-color: lightgreen; padding: 20px;'><h1>🚀 DevOps Master V2!</h1><h2>Wisdom from pod: {host_name}</h2><p>{random.choice(wisdoms)}</p></div>"
+    return f"<div style='background-color: lightgreen; padding: 20px;'><h1>🚀 ofek DevOps Master V2!</h1><h2>Wisdom from pod: {host_name}</h2><p>{random.choice(wisdoms)}</p></div>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
